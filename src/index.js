49cac0af21c1016a2fe34dad79f02b4abac8b78a
netta1989 <132469447+netta1989@users.noreply.github.com>
2023-05-03 19:45:27 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -17,8 +17,23 @@ const fetchYandexData = (token) =>
 
 window.onload = () => {
   document.getElementById("suggest").onclick = () => {
-    // TODO suggest
-  };
+  YaAuthSuggest.init({
+         client_id: '--cdc84ba8cb97478a9519f7f2aadee334--', 
+         response_type: 'token',
+         redirect_uri: '--https://oauth-master-class-one.vercel.app/token.html--'
+      },
+      '--https://oauth-master-class-one.vercel.app/--'
+   )
+   .then(({ handler }) => handler())
+      .then(async (data) => {
+        const result = await fetchYandexData(data.access_token);
+
+        authorize(result);
+
+        console.log(result, data);
+      })
+      .catch((error) => console.log("Что-то пошло не так: ", error));
+      };
   document.getElementById("button").onclick = () => {
     // TODO button
   };
