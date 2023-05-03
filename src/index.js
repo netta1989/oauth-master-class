e4679e33d48ff30b55a56c6bec60b0e77303a802
netta1989 <132469447+netta1989@users.noreply.github.com>
2023-05-03 20:30:16 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
+//Авторизуем пользователя, используя инфу о нем
+const authorize = ({
+  default_avatar_id: defaultAvatarID
+  display_name: displayName,
+}) => {
+  const avatarHtml = `<div class="avatar" style="background-image:url('https://avatars.mds.yandex.net/get-yapic${defaultAvatarID}/islands-middle')"></div>`;
+  const nameHtml = `<div class="name">${displayName}</div>`;
+  
+  document.getElementById("auth").innerHTML = `${avatarHtml}${nameHtml}`;
+};
+
+//Делаем запрос за инфой о пользователе.
+const fetchYandexData = (token) =>
+fetch(`https://login.yandex.ru/info?format=json&oauth_token=${token}`).then(
+  (res) => res.json()
+  );
+
 window.onload = () => {
   document.getElementById("suggest").onclick = () => {
   YaAuthSuggest.init({
@@ -17,3 +34,7 @@ window.onload = () => {
       })
       .catch((error) => console.log("Что-то пошло не так: ", error));
       };
+  document.getElementById("suggest").onclick = () => {
+  //TODO button
+  };
+};
