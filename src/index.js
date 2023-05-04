ea4ceeea322191dbe56b53d4c59d39caed778122
netta1989 <132469447+netta1989@users.noreply.github.com>
2023-05-04 09:54:04 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,40 +1,49 @@
-//Авторизуем пользователя, используя инфу о нем
-const authorize = ({
-  default_avatar_id: defaultAvatarID
-  display_name: displayName,
-}) => {
-  const avatarHtml = `<div class="avatar" style="background-image:url('https://avatars.mds.yandex.net/get-yapic${defaultAvatarID}/islands-middle')"></div>`;
-  const nameHtml = `<div class="name">${displayName}</div>`;
-  
-  document.getElementById("auth").innerHTML = `${avatarHtml}${nameHtml}`;
-};
+<!doctype html>
+<html lang="ru">
 
-//Делаем запрос за инфой о пользователе.
-const fetchYandexData = (token) =>
-fetch(`https://login.yandex.ru/info?format=json&oauth_token=${token}`).then(
-  (res) => res.json()
-  );
+<head>
+<meta charSet="utf-8" />
+<meta name='viewport' content='width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, shrink-to-fit=no, viewport-fit=cover'>
+<meta http-equiv='X-UA-Compatible' content='ie=edge'>
+<style>
+   html,
+   body {
+      background: #eee;
+   }
+</style>
+<script src="https://yastatic.net/s3/passport-sdk/autofill/v1/sdk-suggest-with-polyfills-latest.js"></script>
+</head>
 
-window.onload = () => {
-  document.getElementById("suggest").onclick = () => {
-  YaAuthSuggest.init({
-         client_id: "cdc84ba8cb97478a9519f7f2aadee334", 
-         response_type: "token",
-         redirect_uri: "https://oauth-master-class-one.vercel.app/token.html"
-      },
-      "https://oauth-master-class-one.vercel.app/"
-   )
-   .then(({ handler }) => handler())
-      .then(async (data) => {
-        const result = await fetchYandexData(data.access_token);
-
-        authorize(result);
-
-        console.log(result, data);
-      })
-      .catch((error) => console.log("Что-то пошло не так: ", error));
+<body>
+   <script>
+   window.onload = function() {
+      window.YaAuthSuggest.init({
+               client_id: 'c46f0c53093440c39f12eff95a9f2f93',
+                  response_type: 'token',
+                  redirect_uri: 'https://examplesite.com/suggest/token'
+               },
+               'https://examplesite.com', {
+                  view: 'button',
+                  parentId: 'container',
+                  buttonView: 'main',
+                  buttonTheme: 'light',
+                  buttonSize: 'm',
+                  buttonBorderRadius: 0
+               }
+            )
+            .then(function(result) {
+               return result.handler()
+            })
+            .then(function(data) {
+               console.log('Сообщение с токеном: ', data);
+               document.body.innerHTML += `Сообщение с токеном: ${JSON.stringify(data)}`;
+            })
+            .catch(function(error) {
+               console.log('Что-то пошло не так: ', error);
+               document.body.innerHTML += `Что-то пошло не так: ${JSON.stringify(error)}`;
+            });
       };
-  document.getElementById("suggest").onclick = () => {
-  //TODO button
-  };
-};
+   </script>
+</body>
+
+</html>
