acc05db0deff1421a997167dd8fecca9296f7de2
netta1989 <132469447+netta1989@users.noreply.github.com>
2023-05-04 10:29:43 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,49 +1,64 @@
-<!doctype html>
-<html lang="ru">
+// Авторизуем пользователя, используя инфу о нем.
+const authorize = ({
+  default_avatar_id: defaultAvatarId,
+  display_name: displayName,
+}) => {
+  const avatarHtml = `<div class="avatar" style="background-image:url('https://avatars.mds.yandex.net/get-yapic/${defaultAvatarId}/islands-middle')"></div>`;
+  const nameHtml = `<div class="name">${displayName}</div>`;
 
-<head>
-<meta charSet="utf-8" />
-<meta name='viewport' content='width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, shrink-to-fit=no, viewport-fit=cover'>
-<meta http-equiv='X-UA-Compatible' content='ie=edge'>
-<style>
-   html,
-   body {
-      background: #eee;
-   }
-</style>
-<script src="https://yastatic.net/s3/passport-sdk/autofill/v1/sdk-suggest-with-polyfills-latest.js"></script>
-</head>
+  document.getElementById("auth").innerHTML = `${avatarHtml}${nameHtml}`;
+};
 
-<body>
-   <script>
-   window.onload = function() {
-      window.YaAuthSuggest.init({
-               client_id: 'cdc84ba8cb97478a9519f7f2aadee334',
-                  response_type: 'token',
-                  redirect_uri: 'https://oauth-master-class-one.vercel.app/token.html'
-               },
-               'https://oauth-master-class-one.vercel.app', {
-                  view: 'button',
-                  parentId: 'container',
-                  buttonView: 'main',
-                  buttonTheme: 'light',
-                  buttonSize: 'm',
-                  buttonBorderRadius: 0
-               }
-            )
-            .then(function(result) {
-               return result.handler()
-            })
-            .then(function(data) {
-               console.log('Сообщение с токеном: ', data);
-               document.body.innerHTML += `Сообщение с токеном: ${JSON.stringify(data)}`;
-            })
-            .catch(function(error) {
-               console.log('Что-то пошло не так: ', error);
-               document.body.innerHTML += `Что-то пошло не так: ${JSON.stringify(error)}`;
-            });
-      };
-   </script>
-</body>
+// Делаем запрос за инфой о пользователе.
+const fetchYandexData = (token) =>
+  fetch(`https://login.yandex.ru/info?format=json&oauth_token=${token}`).then(
+    (res) => res.json()
+  );
 
-</html>
+window.onload = () => {
+  document.getElementById("suggest").onclick = () => {
+    YaAuthSuggest.init(
+      {
+        client_id: "4c42d357493f4ca28e5b823d7905f43a",
+        response_type: "token",
+        redirect_uri: "https://oauth-master-class-lake.vercel.app/token.html",
+      },
+      "https://oauth-master-class-lake.vercel.app"
+    )
+      .then(({ handler }) => handler())
+      .then(async (data) => {
+        const result = await fetchYandexData(data.access_token);
+
+        authorize(result);
+
+        console.log(result, data);
+      })
+      .catch((error) => console.log("Что-то пошло не так: ", error));
+  };
+  document.getElementById("button").onclick = () => {
+    window.YaAuthSuggest.init(
+      {
+        client_id: "4c42d357493f4ca28e5b823d7905f43a",
+        response_type: "token",
+        redirect_uri: "https://oauth-master-class-lake.vercel.app/token.html",
+      },
+      "https://oauth-master-class-lake.vercel.app",
+      {
+        parentId: "buttonContainer",
+        view: "button",
+        buttonTheme: "light",
+        buttonSize: "xs",
+        buttonBorderRadius: 20,
+      }
+    )
+      .then(({ handler }) => handler())
+      .then(async (data) => {
+        const result = await fetchYandexData(data.access_token);
+
+        authorize(result);
+
+        console.log(result, data);
+      })
+      .catch((error) => console.log("Что-то пошло не так: ", error));
+  };
+};
